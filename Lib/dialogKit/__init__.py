93a89da237b2bb8a4bcacd88bd79398c96760b0d
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2011-06-08 16:47:51 +0000

    Vanilla works in Glyphs, so reinstate the _dkVanilla import.

--- Lib/dialogKit/__init__.py
+++ Lib/dialogKit/__init__.py
@@ -8,11 +8,18 @@ try:
     haveFL = True
 except ImportError:
     haveFL = False
+try:
+    import vanilla
+    haveVanilla = True
+except ImportError:
+    haveVanilla = False
 # perform the environment specific import
 if haveFL:
     from _dkFL import *
+if haveVanilla:
+    from _dkVanilla import *
 else:
     raise ImportError, 'dialogKit is not available in this environment'
-    
+
 numberVersion = (0, 0, "beta", 1)
 version = "0.0.1b"
