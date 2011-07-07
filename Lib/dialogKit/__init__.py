845192bb91a2421c54a9bbb924e1e09e700aee66
tal <tal@9b05a68b-5939-0410-bc0c-8e10e763eb1c>
2011-07-07 19:35:19 +0000

    Stop trying imports after something has been successfully loaded.

--- Lib/dialogKit/__init__.py
+++ Lib/dialogKit/__init__.py
@@ -3,20 +3,23 @@ dialogKit: easy bake dialogs
 """
 
 # determine the environment
+haveFL = False
+haveVanilla = False
 try:
     import FL
     haveFL = True
 except ImportError:
-    haveFL = False
-try:
-    import vanilla
-    haveVanilla = True
-except ImportError:
-    haveVanilla = False
+    pass
+if not haveFL:
+    try:
+        import vanilla
+        haveVanilla = True
+    except ImportError:
+        pass
 # perform the environment specific import
 if haveFL:
     from _dkFL import *
-if haveVanilla:
+elif haveVanilla:
     from _dkVanilla import *
 else:
     raise ImportError, 'dialogKit is not available in this environment'
